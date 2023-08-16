fbb1ae51e70db9c470cb7168a2aec828d71a3227
ahmedouvadel <ahmedvadel94@gmail.com>
2023-08-16 13:17:48 +0100

    message

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  constructor(
+    private router : Router,
+  ) {}
+
+  event() {
+    this.router.navigate([''])
+  }
+
+  ngOnInit(): void {
+
+  }
 }
