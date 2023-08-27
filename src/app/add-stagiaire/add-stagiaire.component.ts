8002726e90c5bf108d97995c25790637039a593b
ryhab elmoncerr <ryhabelmoncerr@mail.com>
2023-08-27 19:42:34 +0100

    modif 7:40

--- src/app/add-stagiaire/add-stagiaire.component.ts
+++ src/app/add-stagiaire/add-stagiaire.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-stagiaire',
   templateUrl: './add-stagiaire.component.html',
   styleUrls: ['./add-stagiaire.component.css']
 })
-export class AddStagiaireComponent {
-  constructor(public dialogRef: MatDialogRef<AddStagiaireComponent>) {}
+export class AddStagiaireComponent implements OnInit {
+  form: FormGroup<any>;
+  constructor(public dialogRef: MatDialogRef<AddStagiaireComponent>,private fb: FormBuilder) {
+    this.form = new FormGroup({});
+  }
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      nom: ['', Validators.required],
+      prenom: [''],
+      dateDebut: [''],
+      dateFin: [''],
+      service: [''],
+      numeroBadge: [''],
+      numeroTelephone: ['']
+    });
+  }
+
+  submitForm() {
+    if (this.form.valid) {
+      // Handle form submission
+      console.log(this.form.value);
+    }
+  }
   //fct fermer formulaire 
   cancelForm() {
     this.dialogRef.close(); // Fermer le dialogue
